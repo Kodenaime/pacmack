94e577b0cf0a6787b38cf2b8c5fc7bab03859a31
Kodenaime <creativedannyk@gmail.com>
2025-06-01 22:15:05 +0100

    basic folder structure and componenents

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Hero from './components/Hero/Hero'
+import About from './components/About/About'
+import Timer from './components/Timer/Timer'
+import Goal from './components/Goal/Goal'
+import Events from './components/Events/Events'
+import Speakers from './components/Speakers/Speakers'
+import Location from './components/Location/Location'
+import Sponsors from './components/Sponsors/Sponsors'
+import Team from './components/Team/Team'
+import Donate from './components/Donate/Donate'
+import Contact from './components/Contact/Contact'
+import Faqs from './components/Faqs/Faqs'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Hero />
+      <Timer />
+      <About />
+      <Goal />
+      <Events />
+      <Speakers />
+      <Location />
+      <Sponsors />
+      <Team />
+      <Donate />
+      <Contact />
+      <Faqs />
+      <Donate />
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/components/Faqs/Faqs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Faqs = () => {
+  return (
+    <div>
+        <h1>I am FAQS</h1>
+    </div>
+  )
+}
+
+export default Faqs
