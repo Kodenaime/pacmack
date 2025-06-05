463d5ed7204d573a68d92c634e19e6c284054404
Kodenaime <creativedannyk@gmail.com>
2025-06-05 22:45:25 +0100

    starrted with hero and navbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import './hero.css'
 import Navbar from '../Navbar/Navbar'
 
 const Hero = () => {
   return (
     <header>
       <Navbar />
+      <div className="hero">
+        <div className="hero-time">
+          1st - 5th Oct 2025, Lagos, Nigeria
+        </div>
+      </div>
     </header>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './navbar.css'
+
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
     <div>
-        <h1>I am Nav</h1>
+      <nav className='container'>
+        <a className="logo" href='/'>
+          <img src={logo} alt="Pacmack Logo" />
+        </a>
+        <ul className='nav-items'>
+          <li className="nav-item"><a href="#about">About</a></li>
+          <li className="nav-item"><a href="#events">Events</a></li>
+          <li className="nav-item"><a href="#venue">Venue</a></li>
+          <li className="nav-item"><a href="#contact">Contact Us</a></li>
+        </ul>
+    </nav>
     </div>
   )
 }
